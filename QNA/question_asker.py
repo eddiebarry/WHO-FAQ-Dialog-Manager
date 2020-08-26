fa5595b5176040768b22ec0500234dc5d8829a2c
eddiebary <edgarmonis@gmail.com>
2020-08-26 11:12:52 +0000

    changing format of qna manager

---
+++ QNA/question_asker.py
@@ -0,0 +1,9 @@
+# Get Question
+    # Check if id is present
+
+# Extract Keywords
+    # merge with previous if value is not sentinel
+
+# Check if keywords are present, if yes answer
+
+# If not, Store keywords, return question id, return specific question
